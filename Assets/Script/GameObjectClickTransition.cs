45b5f3ba9f1b7a51d2cbcf806fc4f7020e36cf3c
Inhwa Yeom <yinhwa.erica@gmail.com>
2020-04-27 12:21:39 +0900

    scene transition onClick 학술문화관

---
+++ Assets/Script/GameObjectClickTransition.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameObjectClickTransition : MonoBehaviour
+{
+
+
+
+    // 학술문화관 건물 클릭 후 곧바로 LoadScene 불러오기
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("CuratorMode");
+    }
+
+
+
+    //public string sceneName = "CuratorMode"; //이동할 Scene
+
+
+    //void Update()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        RaycastHit hit;
+    //        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+    //        if (Physics.Raycast(ray, out hit, 100.0f))
+    //        {
+    //            if (hit.transform != null)
+    //            {
+    //                PrintName(hit.transform.gameObject);
+    //            }
+    //        }
+
+    //    }
+
+    //}
+
+    //// Update is called once per frame
+    //private void PrintName(GameObject go)
+    //{
+    //    print(go.name);
+    //}
+    //public void ClickStart(Rigidbody KAIST_Campus_학술문화관)
+    //{
+    //    Debug.Log("로딩");
+    //    SceneManager.LoadScene(sceneName);
+    //}
+}
